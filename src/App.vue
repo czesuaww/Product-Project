a333e9021b59f7a53e5849e03c7b81e8c495e42b
czesuaww <greg152295@gmail.com>
2026-01-28 09:53:19 +0100

    change for routerview from homeview

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,8 @@
 <script setup lang="ts">
-import HomeView from './views/HomeView.vue';
 </script>
 
 <template>
   <main class="container">
-    <HomeView />
+    <RouterView />
   </main>
 </template>
